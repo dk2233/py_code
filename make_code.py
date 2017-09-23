eabeac20024a17826bea6ab48a6e719218c47550
daniel <daniel@inmoss-MacBook-Pro.local>
2017-09-23 16:11:22 +0200

    i make it workable and it can read all function in form of long line, now I need to change how this class is initializated

--- make_code.py
+++ make_code.py
@@ -17,21 +17,21 @@ MODULE_PREFIX   =   "EDR"
 
 
 AllAnalysedVariableTypes_dict = {
-    "typedef":      "_t",
-    "uint8_t":      "_u8",
-    "uint8":        "_u8", 
-    " int8_t":       '_i8',
-    " int8":         '_i8',
-    "uint16_t":     "_u16",
-    "uint16":     "_u16",
-    " int16_t":     "_i16",
-    " int16":     "_i16",
-    "uint32_t":     "_u32",
-    "uint32":     "_u32",
-    " int32_t":     "_i32",
-    " int32":     "_i32",
-    "boolean_t":    "_bo",
-    "bool_t":       "_bo",
+    "typedef":          "_t",
+    "uint8_t":          "_u8",
+    "uint8":            "_u8", 
+    " int8_t":          '_i8',
+    " int8":            '_i8',
+    "uint16_t":         "_u16",
+    "uint16":           "_u16",
+    " int16_t":         "_i16",
+    " int16":           "_i16",
+    "uint32_t":         "_u32",
+    "uint32":           "_u32",
+    " int32_t":         "_i32",
+    " int32":           "_i32",
+    "boolean_t":        "_bo",
+    "bool_t":           "_bo",
 }
 AllEquality_tab = {
     "=",
@@ -52,6 +52,19 @@ FinishMarkers  = (
     "\\"
 )
 
+SignNotForWord = (
+    ";",
+    "{",
+    "}",
+    "]",
+    "[",
+    ",",
+    "="
+    
+    
+)
+
+AllFunctionArgument = {}
 
 ######################################################
 # DEFINITIONS
@@ -81,7 +94,7 @@ class ClassToAnalyseCfile:
             print(" I opened ",filename+".c") 
             print("!"*100)
             line = self.file_c_p.readline()
-           
+            
             while(line):
                 self.tab_c.append(line)
                 line = self.file_c_p.readline()
@@ -111,7 +124,8 @@ class ClassToAnalyseCfile:
         self.InTypedefBlockDefinition = False
         #self.InTypedefLineDefinition = False
         self.AnalyseThatVariable = False
-        
+        self.FunctionPrototype = False
+        self.InFunctionDeclaration = False
         
         
     def ShowCfile(self,tab_from_c_file):
@@ -125,6 +139,7 @@ class ClassToAnalyseCfile:
         and line without comments"""
         
         line=tab[self.nr_line]
+        #print(self.nr_line,"\n",line)
         line_wc=line
         if (START_COMMENT_BLOCK in line):
         #and ( not END_COMMENT_BLOCK in line):
@@ -134,23 +149,14 @@ class ClassToAnalyseCfile:
                 self.nr_line +=1
                 line +=tab[self.nr_line]
                 
-            #line +=tab[self.nr_line]    
-            #print(self.nr_line," line ",line)
             line_wc=line[:line.find(START_COMMENT_BLOCK)]+line[line.find(END_COMMENT_BLOCK)+len(END_COMMENT_BLOCK):]
-            #print(" wc ",line_wc)
-        # elif  (START_COMMENT_BLOCK in line) and (  END_COMMENT_BLOCK in line):
-            # self.NextLineIsCommented = False
-            # line_wc=line[:line.find(START_COMMENT_BLOCK)]+line[line.find(END_COMMENT_BLOCK)+len(END_COMMENT_BLOCK):]
-        # elif ( (END_COMMENT_BLOCK in line) and (self.NextLineIsCommented is True)):
-            # self.NextLineIsCommented = False
-            # line_wc=line[line.find(END_COMMENT_BLOCK)+len(END_COMMENT_BLOCK):]
         elif r'//' in line:
         #and not any(element in line for element in WordWithoutEndingCharacters):
             line_wc=line[:line.find('//')]
             
         return line,line_wc
      
-     
+    
     def DecodeCline(self,line):
         """ method to decode line """
     
@@ -194,30 +200,15 @@ class ClassToAnalyseCfile:
     
     def FindVariableinDefinition(self,line):
         """ here I have to give line without any comments """
-        # print(line)
         
-        #print(line_tab)
-#        if "{" in line and "}" in line: 
-#            line = line[:line.find("{")] + line[line.find("}")+1:]
-#            print(line)
-            
-        #LineFinish = False
         if "typedef" in line:
-            while line.count("{")>0 and line.count("}")>0:
-                #print(line)
+            while line.count("{")>0 and line.count("}")>0:        
                 a1 = line.find("{")
-                
                 a2 = len(line)-1
                 while line[a2] is not "}":
                     a2 -=1
                 line = line[:a1]+line[a2+1:]
-                #print(line)
-
-            #input(" after merging ")
-        
-        
-        #else:
-            
+                
         line_tab = self.DivideLine(line)
 
         if "=" in line:
@@ -240,18 +231,18 @@ class ClassToAnalyseCfile:
         return( variable)
     
     
+    
     def  AddPrefixToVariable(self,variable,prefix,line):
         #wrong prefix:
-        
+        new_var = variable
         for pref in AllAnalysedVariableTypes_dict.values():
             if pref is prefix:
                 #if same prefix it is ok to be in variable
                 continue
             # print(pref)
-            if (pref in variable[(len(variable) - len(pref)): len(variable)]) :
+            if (pref in variable[(len(variable) - len(pref)): len(variable)]):
                 variable = variable.replace(pref,"")
-                # input("key")
-        
+                 
         #not real prefix - remove obsolete _xx (two letter max)
         if not (prefix in variable[(len(variable) - len(prefix)): len(variable)]) :
             i=variable[len(variable)-2:].find("_")
@@ -259,36 +250,76 @@ class ClassToAnalyseCfile:
                 variable = variable[:i]
                 
             new_var = variable+prefix
-            print(new_var)
             self.dict_of_Variables_to_change[variable]=new_var
                 
             print(" I am adding new variables to dictionary of ")
             
             line = line.replace(variable,new_var)
             print(" after change ",line)
-            #break
-#                        line_tab[i] = new_var
-#                        line=""
-#                        for i in line_tab:
-#                            line = line+i
-#                        line=line+"\n"  
         else:
             print(" prefix ", prefix," already in variable ")
             
-            
         if "enum" in line and "typedef" in line:
             AllAnalysedVariableTypes_dict[variable]="_e"
-            
-                
-        # print("#"*100)
-        # print(variable)
-        # print("#"*100)
-        return(line)
+    
+        return new_var,line
     
     
-    def IsFunctionInLine(self,line):
+    def FunctionPrototypeInLine(self,string):
         """ to find all functions """
         
+        function_name =""
+        
+        string = string.replace("("," ( ")
+        string = string.replace(")"," ) ")
+        print(string)
+        
+        temp = string.split()
+        i=0
+        ArgumentSearch = False
+        tab_arg = []
+        arg = []
+        print(temp)
+        while i< len(temp):
+            print(temp[i])
+            # if ArgumentSearch is True and 
+                # break
+            
+            if ArgumentSearch is True:
+                
+                print("\t\t",temp[i])
+                print(arg)
+                #print(" comparing ", temp[i] == 'void' ,temp[i],'void')
+                if temp[i] == 'void':
+                    print(" exit ")
+                    break
+                elif temp[i] is not ',' and temp[i] is not ')':    
+                    arg.append(  temp[i])
+                else:    
+                    #print(arg)
+                    print(" variable ",arg[len(arg)-1]," of type ",arg[len(arg)-2])
+                    
+                    tab_arg.append([arg[len(arg)-2], arg[len(arg)-1]])
+                    if temp[i] is ")":
+                        break
+                    
+            if temp[i] is "(" :            
+                function_name = temp[i-1]
+                ArgumentSearch = True
+            
+            i+=1
+        return function_name, tab_arg
+
+    
+    
+    
+    
+    def IfPointerAdd_p(self,variable,prefix):
+        if "*" in variable[0]:
+            prefix = prefix.replace("_","_p")
+        return prefix    
+    
+    
     
     
     def FindAllExternInH(self):
@@ -298,35 +329,106 @@ class ClassToAnalyseCfile:
         while iter < len(self.tab_h):
             line=self.tab[iter]
             line = self.CorrectCLine(line)
-            
             iter +=1
-    
-    
+
+            
+            
+            
+    def IsThisWord(self,string):
+        isWord = False
+        if len(string)>2:
+            if not any(mark in string for mark in SignNotForWord):
+                isWord = True
+        return isWord  
+                
+
+        
+        
+        
     def MergeLineInDifferentLines(self,tab):
-        """ method to connect all lines that are one statement, but divided during development for readibility """
+        """ method to connect all lines that are one statement, but divided during development for readibility 
+        
+        it sees that:
+        - line has ';' that is simple statement but couple of lines can be one statemen
+        - line can has function prototype it has () and ;
+        - line can start function definitions in that case we have more then two words + ()+ { and ending }
+        """
+        
         #line=tab[self.nr_line]
-        line,line_without_comment = self.CheckWhetherLineCommented(self.tab_c)
-        temp = line
+        
+        temp = ""
         level_of_para = 0
+        level_of_para_func = 0
         #while not any(element in line_without_comment for element in FinishMarkers ):
-        
+        self.FunctionPrototype = False
+        wordCountBeforeParanthesis = 0
+        #wordNumber = 0
         LineFinish = False
-        if ';' in line:
-            LineFinish = True
-        
+        self.InFunctionDeclaration = False
+#        if ';' in line:
+#            LineFinish = True
+       
         while not LineFinish:
-            #print(line)
-            self.nr_line +=1
             line,line_without_comment = self.CheckWhetherLineCommented(self.tab_c)
+            #print(line, "\t",AllFunctionArgument)
+        
+            if  any(func in line for func in AllFunctionArgument):
+                print(" that function ",func, " is in tab functions ")
+                #IamInFunction = True
+                input(" func ")
+            
             temp +=line
+            line_without_comment = line_without_comment.replace("("," ( ")
+            line_without_comment = line_without_comment.replace(")"," ) ")
+            
+            t1  = ( line_without_comment.strip() ).split()
+            for word in t1:
+                if self.InFunctionDeclaration == True:
+                    wordCountBeforeParanthesis = 0
+                    break
+                    
+                if "{" in word and "(" in temp and ")" in temp:
+                    if wordCountBeforeParanthesis >=2 :
+                        self.InFunctionDeclaration = True
+                        print(temp)
+                        #LineFinish = True
+                        input(" i am in the function ")
+                
+                if self.IsThisWord(word) == True:
+                    wordCountBeforeParanthesis +=1
+#                elif "(" in word:
+#                    wordCountBeforeParanthesis = 0
+#            
             
             level_of_para += line_without_comment.count("{")
             level_of_para -= line_without_comment.count("}")
             
-            if ';' in line and level_of_para is 0:
+            # level_of_para_func += line_without_comment.count("(")
+            # level_of_para_func -= line_without_comment.count(")")
+                
+            if ';' in line and (level_of_para == 0) and self.InFunctionDeclaration == False:
                 LineFinish = True
-        #print(temp)
+                break
             
+            if self.InFunctionDeclaration == True and (level_of_para == 0) and "}" in line:
+                LineFinish = True
+                break
+                
+            self.nr_line +=1
+                      
+                #LineFinish = True
+        print("*"*100)                    
+        print(" merge :",temp)
+        print("^"*100)
+        if "(" in temp and ";" in temp and not '{' in temp:
+            self.FunctionPrototype = True
+            #print(temp)
+            print(temp.replace("\n"," "))
+            temp = temp.replace("\n"," ")
+            temp +="\n"
+            #input(" func ")
+            # line=tab[self.nr_line]
+        # print(self.nr_line,"\n",line)
         #input(" after merging ")
         return(temp)    
        
@@ -339,7 +441,7 @@ class ClassToAnalyseCfile:
             #line=self.tab_c[self.nr_line]      
             
             line,line_without_comment = self.CheckWhetherLineCommented(self.tab_c)
-            print(self.nr_line,"\n wc ->:",line_without_comment,"\n\t",line)
+            print(self.nr_line,"\n wc ->:",line_without_comment,"\n\t",line,end='')
             #print(" w c")
             #input( " key ")
             if len(line_without_comment.strip())<2:
@@ -347,6 +449,9 @@ class ClassToAnalyseCfile:
                 fileHandler_tmp.write(line)
                 self.nr_line+=1 
                 continue
+            
+            
+            
             
             if any(element in line_without_comment for element in WordWithoutEndingCharacters):
                 #print(line)
@@ -372,6 +477,32 @@ class ClassToAnalyseCfile:
             
             line = self.MergeLineInDifferentLines(self.tab_c)
             
+            if self.FunctionPrototype is True:
+                func_name,tab_arg = self.FunctionPrototypeInLine(line)
+                for arg in tab_arg:
+                    print("arg ",arg)
+                    
+                    print(AllAnalysedVariableTypes_dict)
+                    
+                    
+                    if arg[0] in AllAnalysedVariableTypes_dict:
+                        prefix = AllAnalysedVariableTypes_dict[arg[0]]
+                    
+                        new_var, line  = self.AddPrefixToVariable(arg[1],prefix,line)
+                        #print(new_var)
+                        AllFunctionArgument[func_name]={arg[1]:new_var}
+                print(func_name)
+                print(AllFunctionArgument)
+                fileHandler_tmp.write(line)
+                self.nr_line+=1 
+                continue
+                input( " func ")
+                
+            
+            if self.InFunctionDeclaration == True:
+                pass
+            
+            
             
             # if any(word in line for word in WordWithoutEndingCharacters):
                 
@@ -380,10 +511,10 @@ class ClassToAnalyseCfile:
             #line = self.CorrectCLine(line)
             #print(line)
             
-            print(line)
+            
             
             #print(line_without_comment)
-            # input( " key ")
+            #input( " key ")
             line_tab = self.DivideLine(line)
             
             
@@ -394,7 +525,7 @@ class ClassToAnalyseCfile:
                 var = self.FindVariableinDefinition(line)
                 print(" var ",var)
                 
-                line = self.AddPrefixToVariable(var,AllAnalysedVariableTypes_dict["typedef"],line)
+                new_var,line  = self.AddPrefixToVariable(var,AllAnalysedVariableTypes_dict["typedef"],line)
                 
                 #line = line[]
                 
@@ -417,11 +548,6 @@ class ClassToAnalyseCfile:
                         NextValue = True
                         line = line.replace(var_is_already, self.dict_of_Variables_to_change[var_is_already] )
                         break
-
-#                if NextValue is True:
-#                    fileHandler_tmp.write(line)
-#                    self.nr_line+=1 
-#                    continue 
                 
                 for var_type in AllAnalysedVariableTypes_dict.keys():
                     
@@ -430,15 +556,16 @@ class ClassToAnalyseCfile:
                         print(var_type," line = ",line_without_comment)
                         variable = self.FindVariableinDefinition(line_without_comment)
                         prefix = AllAnalysedVariableTypes_dict[var_type]
-                        if '*' in line:
-                            print(prefix,end='')
-                            #check if this is pointer variable
-                            prefix = prefix.replace('_','_p')
-                            print(" new :",prefix)
-                            #input(" pointer ")
+                        prefix = self.IfPointerAdd_p(variable,prefix)
+                        # if '*' in line:
+                            # print(prefix,end='')
+                            # prefix = prefix.replace('_','_p')
+                            # print(" new :",prefix)
+                            
+                    
                         print(" prefix for variable ",prefix)
                         #i am checking if this prefix is not already in variable
-                        print(variable)
+                        #print(variable)
                         if not (prefix in variable[(len(variable) - len(prefix)): len(variable)]) :
                             new_var = variable+prefix
                             print(new_var)
